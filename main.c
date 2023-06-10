88e690a83e6c648b40fcdec95cc5d4cb239d63e7
Lucas <lucasdamata01@hotmail.com>
2023-06-10 12:41:48 -0300

    reestoque

--- main.c
+++ main.c
@@ -1,11 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tabelas.h"
 #include "estoque.h"
 
-int main(void){
-    int qtds[] = {10,10,10,10,10};
-    Estoque estoque = iniciarEstoque(qtds);
+typedef struct Reestoque
+{
+    //Chega em 3 dias
+    int bebida_A;
+    int bebida_B;
+    //Feito todo dia
+    int pao;
+    //Recebe a cada 2 dias
+    int hamburguer_frango;
+    //Recebe diariament
+    int hamburguer_carne;
+} Reestoque;
 
-    printf("%d", estoque.hamburgue_carne);
-    scanf("%d");
+Reestoque iniciarRestoque(){
+    Reestoque reestoque;
+
+    reestoque.bebida_A = 300;
+    reestoque.bebida_B = 200;
+    reestoque.pao = 40;
+    reestoque.hamburguer_frango = 250;
+    reestoque.hamburguer_carne = 320;
+}
+
+int restoqueBebidaA(Reestoque reestoque){
+    return reestoque.bebida_A;
+}
+int restoqueBebidaB(Reestoque reestoque){
+    return reestoque.bebida_B;
+}
+int restoquePao(Reestoque reestoque){
+    return reestoque.pao;
+}
+int restoqueHamburguerFrango(Reestoque reestoque){
+    return reestoque.hamburguer_frango;
+}
+int restoqueHamburguerCarne(Reestoque reestoque){
+    int desvioPadrao = 30;
+    int media = reestoque.hamburguer_carne;
+
+    int menor = media - desvioPadrao;
+    int maior = media + desvioPadrao;
+
+    int quantidade = (rand() % (maior - menor + 1)) + menor;
+    return quantidade;
+}
+
+int main(void){
+    
 }
